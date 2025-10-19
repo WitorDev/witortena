4a52844fbd154ae44f52fc44716f546d79b6c32b
witortenadev <witortena@edu.unifil.br>
2025-10-19 18:52:12 -0300

    type error fixed

--- app/components/IconAndTitle.tsx
+++ app/components/IconAndTitle.tsx
@@ -1,6 +1,6 @@
 import * as SiIcons from "react-icons/si";
 import * as MdIcons from "react-icons/md";
-import { ReactNode } from "react";
+import { IconType } from "react-icons"; // 1. Import IconType
 
 type IconAndTitleProps = {
   title: String;
@@ -9,10 +9,13 @@ type IconAndTitleProps = {
 export default function IconAndTitle({ title }: IconAndTitleProps) {
   const iconSet = title === "Email" ? MdIcons : SiIcons;
   const iconName = title === "Email" ? "MdEmail" : `Si${title}`;
-  const IconComponent: ReactNode = iconSet[iconName as keyof typeof iconSet];
+
+  // 2. Use IconType instead of ReactNode
+  const IconComponent: IconType = iconSet[iconName as keyof typeof iconSet];
 
   return (
     <div className="p-4 w-full flex gap-4 items-center bg-primary-bg mt-8">
+      {/* This will now work correctly */}
       {IconComponent && <IconComponent size={50} />}
       <h2 className="text-2xl font-bold">{title}</h2>
     </div>
