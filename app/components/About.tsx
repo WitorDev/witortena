4baa3865082aa5d17508b6ba82aed7cb0dd0b3ee
witortenadev <witortena@edu.unifil.br>
2026-01-06 20:05:56 -0300

    updated about page layout and style

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 import { Ubuntu_Mono } from "next/font/google";
 import { FaGithub, FaGithubSquare } from "react-icons/fa";
 import { HiDocument, HiDocumentDownload } from "react-icons/hi";
+import { CgClose } from "react-icons/cg";
+import { BiWorld } from "react-icons/bi";
 
 const ubuntuMonoFont = Ubuntu_Mono({
   subsets: ["latin"],
@@ -25,106 +27,111 @@ export default function About() {
       className="sm:bg-gradient-to-br bg-gradient-to-b sm:to-primary-accent/10 to-primary-accent/15 from-secondary-bg border-b-2 border-b-secondary-bg border-t-2 border-primary-bg"
     >
       <section
-        className={`sm:${ubuntuMonoFont.className} max-w-screen-xl px-4 mx-auto relative pb-20 md:flex-row-reverse flex-col-reverse flex gap-8 sm:justify-between`}
+        className={`sm:${ubuntuMonoFont.className} max-w-screen-xl justify-between gap-8 px-4 mx-auto relative pb-20 md:flex-row-reverse flex-col-reverse flex sm:justify-between`}
       >
         <motion.div
           initial={{ opacity: 0, y: 50, scale: 0.9 }}
           whileInView={{ opacity: 1, y: 0, scale: 1 }}
           transition={{ duration: 1 }}
           viewport={{ once: true, amount: 0.1 }}
-          className="z-10 mt-20 left-0 sm:mx-0 md:mx-0 md:w-fit mx-auto w-full"
+          className="z-10 md:mt-20 md:min-w-[300px] lg:min-w-[500px] left-0 md:mx-0 md:w-fit mx-auto max-w-[450px]"
         >
           <Image
             alt="Witor Tenã"
-            className="rounded-lg nodrag mx-auto"
+            className="rounded-lg border-2 border-terciary-accent w-full nodrag mx-auto"
             width={500}
             src={photo}
           />
         </motion.div>
-        <div className="mx-auto sm:mx-0 flex flex-col justify-between items-left">
-          <div className="">
-            <motion.h1
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-              viewport={{ once: true, amount: 0.1 }}
-              className="text-3xl font-bold w-full text-center md:text-left max-w-screen-xl pt-20 mx-auto"
-            >
-              Sobre Mim
-            </motion.h1>
 
-            <motion.h1
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-              viewport={{ once: true, amount: 0.1 }}
-              id="name"
-              className={`${ubuntuSansMonoFont.className} mt-4 mb-4 text-terciary-bg w-full mx-auto text-lg  sm:text-4xl text-center md:text-left`}
-            >
-              Witor Tenã
-            </motion.h1>
-
-            <div className="flex gap-4 flex-col">
-              <motion.p
+        <div className="border-2 bg-primary-bg border-terciary-accent rounded-lg mt-20 md:flex-row-reverse flex-col-reverse flex sm:justify-between">
+          <div className="mx-auto sm:mx-0 flex flex-col justify-between items-left">
+            <div className="">
+              <motion.h1
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1 }}
                 viewport={{ once: true, amount: 0.1 }}
-                id="paragraph"
-                className={`${ubuntuSansMonoFont.className} md:max-w-140 text-lg w-full mx-auto`}
+                className="text-3xl flex align-middle bg-terciary-accent justify-between p-4 border-b-2 border-terciary-accent font-bold w-full text-center md:text-left max-w-screen-xl mx-auto"
               >
-                Sou um curioso sobre tecnologia que teve o primeiro contato com
-                a programação ainda pequeno. Tudo começou com Python, e até hoje
-                me lembro da animação dos meus primeiros passos no meio da
-                programação. Desde então, nunca mais parei.
-              </motion.p>
-              <motion.p
+                <h1>Sobre Mim</h1>
+                <h2 className="pr-1 pt-1">
+                  <BiWorld />
+                </h2>
+              </motion.h1>
+              <motion.h1
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1 }}
                 viewport={{ once: true, amount: 0.1 }}
-                id="paragraph"
-                className={`${ubuntuSansMonoFont.className} md:max-w-140 text-lg mb-4 lg:mb-0 w-full mx-auto`}
+                id="name"
+                className={`${ubuntuSansMonoFont.className} bg-primary-bg pt-6 p-4 text-terciary-bg w-full mx-auto text-lg  sm:text-4xl text-left`}
               >
-                O que era apenas curiosidade se transformou em um hábito, e
-                agora é minha missão: programar, evoluir constantemente e
-                absorver o máximo de conhecimento possível nesse universo em
-                constante expansão que é o mundo da tecnologia.
-              </motion.p>
+                Witor Tenã
+              </motion.h1>
+
+              <div className="flex flex-col bg-primary-bg">
+                <motion.p
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1 }}
+                  viewport={{ once: true, amount: 0.1 }}
+                  id="paragraph"
+                  className={`${ubuntuSansMonoFont.className} px-4 text-lg w-full mx-auto`}
+                >
+                  Sou um curioso sobre tecnologia que teve o primeiro contato
+                  com a programação ainda pequeno. Tudo começou com Python, e
+                  até hoje me lembro da animação dos meus primeiros passos no
+                  meio da programação. Desde então, nunca mais parei.
+                </motion.p>
+                <motion.p
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1 }}
+                  viewport={{ once: true, amount: 0.1 }}
+                  id="paragraph"
+                  className={`${ubuntuSansMonoFont.className} p-4 text-lg mb-4 lg:mb-0 w-full mx-auto`}
+                >
+                  O que era apenas curiosidade se transformou em um hábito, e
+                  agora é minha missão: programar, evoluir constantemente e
+                  absorver o máximo de conhecimento possível nesse universo em
+                  constante expansão que é o mundo da tecnologia.
+                </motion.p>
+              </div>
             </div>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true, amount: 0.1 }}
+              className="flex p-4 pb-8 gap-4 m-0 w-full md:mx-0 md:justify-normal flex-row mx-auto sm:translate-0 items-center sm:flex-row md:w-fit"
+            >
+              <a
+                id="github"
+                href="https://github.com/WitorDev/"
+                target="_blank"
+                className="text-primary-accent flex items-center justify-between hover:cursor-pointer hover:text-secondary-accent font-bold"
+              >
+                <div className="flex justify-center align-middle">
+                  <FaGithub size={30} className="" />
+                </div>
+                <p className="py-1 px-2">Github</p>
+              </a>
+              <a
+                id="curriculum_vitae"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="/curriculum.pdf"
+                download={"Curriculum_Witor_Tenã"}
+                className="text-primary-accent flex ml-4 sm:ml-0 items-center justify-between hover:cursor-pointer hover:text-secondary-accent font-bold"
+              >
+                <div className="flex justify-center align-middle">
+                  <HiDocumentDownload size={36} className="" />
+                </div>
+                <p className="py-1 px-2">Currículo</p>
+              </a>
+            </motion.div>
           </div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            viewport={{ once: true, amount: 0.1 }}
-            className="flex gap-4 p-0 m-0 w-full md:mx-0 md:justify-normal flex-row mx-auto sm:translate-0 items-center sm:flex-row md:w-fit"
-          >
-            <a
-              id="github"
-              href="https://github.com/WitorDev/"
-              target="_blank"
-              className="text-primary-accent flex items-center justify-between hover:cursor-pointer hover:text-secondary-accent font-bold"
-            >
-              <div className="flex justify-center align-middle">
-                <FaGithub size={30} className="" />
-              </div>
-              <p className="py-1 px-2">Github</p>
-            </a>
-            <a
-              id="curriculum_vitae"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="/curriculum.pdf"
-              download={"Curriculum_Witor_Tenã"}
-              className="text-primary-accent flex ml-4 sm:ml-0 items-center justify-between hover:cursor-pointer hover:text-secondary-accent font-bold"
-            >
-              <div className="flex justify-center align-middle">
-                <HiDocumentDownload size={36} className="" />
-              </div>
-              <p className="py-1 px-2">Currículo</p>
-            </a>
-          </motion.div>
         </div>
       </section>
     </div>
