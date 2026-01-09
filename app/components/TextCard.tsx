6cf88d420f5702947dccfbf662dd8608c0dd6fa4
witortenadev <witortena@edu.unifil.br>
2026-01-09 17:16:14 -0300

    updated styling of textcard component

--- app/components/TextCard.tsx
+++ app/components/TextCard.tsx
@@ -38,23 +38,23 @@ export default function TextCard({
               link && "hover:cursor-pointer"
             } flex flex-col gap-6 p-8 pb-12 justify-between h-full`}
           >
-            <div className="">
-              <div className="flex gap-4 mb-6 flex-col">
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-4 flex-col">
                 <h1 className="text-lg pr-8">{title}</h1>
                 <p className="text-terciary-bg">{status}</p>
               </div>
               <div className="flex gap-4 flex-col">
                 <p className="text-lg">{description}</p>
               </div>
+              <div className="text-primary-accent flex gap-2 align-middle group-hover:text-green-500">
+                <RiExternalLinkLine size={20} />
+                <p className="">Ver mais...</p>
+              </div>
             </div>
             <div className="flex flex-col gap-4">
               <p className="text-terciary-bg border w-fit border-terciary-bg rounded-2xl px-4">
                 {info}
               </p>
-              <div className="text-primary-accent flex gap-2 align-middle group-hover:text-green-500">
-                <RiExternalLinkLine size={20} />
-                <p className="">Ver mais...</p>
-              </div>
             </div>
           </div>
         </Link>
