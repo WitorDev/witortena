ec1739b06df21739b4265b9294780534857b57f7
witortenadev <witortena@edu.unifil.br>
2026-01-10 22:29:02 -0300

    updated animation in about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -34,7 +34,7 @@ export default function About() {
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3 }}
-          viewport={{ once: true, amount: 0.1 }}
+          viewport={{ once: true }}
           className="z-10 rounded-lg border border-terciary-accent h-fit md:mt-20 md:min-w-[300px] lg:min-w-[500px] left-0 md:mx-0 md:w-fit mx-auto max-w-[450px]"
         >
           <Image
