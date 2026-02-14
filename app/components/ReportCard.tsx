ef20fc17147d663f3fa030ea8133559c3e190f98
witortenadev <witortena@edu.unifil.br>
2026-02-14 11:51:22 -0300

    fix: fixed issue where npi reportcards would not show their paragraph's text

--- app/components/ReportCard.tsx
+++ app/components/ReportCard.tsx
@@ -28,6 +28,7 @@ export default function ReportCard({
   image,
   type,
 }: ReportCardProps) {
+  const cartTitle = paragraph.split("`")[0].substring(0, 37);
   return (
     <div className="transition-all min-w-[300px] max-w-[400px] overflow-hidden flex flex-col h-[500px]">
       <Link href={type + "/" + date.trim().toLowerCase()}>
@@ -50,25 +51,31 @@ export default function ReportCard({
       <div className="px-4 py-6 flex flex-col flex-1">
         <div className="flex justify-between items-start">
           <div className="flex justify-between w-full">
-            <Link
+            {/* <Link
               href={type + "/" + date.trim().toLowerCase()}
               className="flex flex-row-reverse justify-between hover:text-secondary-accent w-full"
             >
-              <RiExternalLinkLine className="transition-all z-10" size={25} />
-              <div className="text-lg font-bold z-1">
-                <MarkdownSection
-                  style={false}
-                  text={paragraph.split("`")[0].substring(0, 37)}
-                />
-              </div>
-            </Link>
+            <RiExternalLinkLine className="transition-all z-10" size={25} />
+            <div className="text-lg font-bold z-1">
+            <MarkdownSection style={false} text={cartTitle} />
+            </div>
+            </Link> */}
           </div>
         </div>
 
-        <p className="text-terciary-bg mb-4">{date.split("_")[1]}</p>
+        <div className="flex justify-between">
+          <p className="text-terciary-bg mb-4">{date.split("_")[1]}</p>
+          <Link
+            href={type + "/" + date.trim().toLowerCase()}
+            className="flex gap-2 hover:text-secondary-accent text-terciary-bg transition-all"
+          >
+            <p>Ver mais</p>
+            <RiExternalLinkLine className="z-10" size={20} />
+          </Link>
+        </div>
 
         <div className="line-clamp-[6]">
-          <ReactMarkdown>{"P" + paragraph.split("`P")[1]}</ReactMarkdown>
+          <ReactMarkdown>{paragraph}</ReactMarkdown>
         </div>
       </div>
     </div>
